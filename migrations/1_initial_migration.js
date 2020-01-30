c55e41719cbb7c088266f963cefc38d2d48ea8e5
Matías Barrios <matiasbn@MacBook-Air-de-Matias.local>
2020-01-30 13:42:45 -0300

    feat: deploying to ropsten

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,9 @@
-const Migrations = artifacts.require("Migrations");
+const Migrations = artifacts.require('Migrations');
+const CoinFlipSol = artifacts.require('CoinFlipSol');
 
-module.exports = function(deployer) {
+const flipCoinAddress = '0x8a90c284dcf34f61f79279a3f442fd9b9c2a875e';
+
+module.exports = function (deployer) {
   deployer.deploy(Migrations);
+  deployer.deploy(CoinFlipSol, flipCoinAddress);
 };
