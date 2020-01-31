407fce4357297c06841e039ffc5bba8803beaf0c
Matías Barrios <matiasbn@MacBook-Air-de-Matias.local>
2020-01-31 14:02:51 -0300

    feat: test finished

--- test/testContract.test.js
+++ test/testContract.test.js
@@ -1,44 +1,47 @@
 /* eslint-disable no-underscore-dangle */
-const BadKing = artifacts.require('BadKing');
-const King = artifacts.require('King');
+const SolArtifact = artifacts.require('ReentranceSol');
+const TargetArtifact = artifacts.require('Reentrance');
 const { catchRevert } = require('./exceptionsHelpers.js');
 
 
-contract('BadKing', (accounts) => {
-  let badKing;
-  let king;
+contract('Reentrance', (accounts) => {
+  let solContract;
+  let targetContract;
+  const initialBalance = web3.utils.toWei('1');
+  const steal = web3.utils.toWei('0.5');
+  const totalBounty = web3.utils.toWei('1.5');
   const owner = accounts[0];
-  const user = accounts[1];
 
   before(async () => {
-    king = await King.new({
-      value: web3.utils.toWei('1'),
-    });
-    badKing = await BadKing.new(king.address, {
-      // more than 1 ether for the transaction gas
-      value: web3.utils.toWei('1'),
-    });
+    targetContract = await TargetArtifact.new();
+    await targetContract.sendTransaction({ value: initialBalance });
+    solContract = await SolArtifact.new(targetContract.address, steal);
   });
 
 
-  it('should change the king to BadKing address', async () => {
-    const prize = web3.utils.toWei('1');
-    const oldKing = await king._king();
-    assert.equal(oldKing, owner, 'oldKing should be the king address');
-    await badKing.dethroneKing(prize);
-    const newKing = await king._king();
-    assert.equal(newKing, badKing.address, 'newKing should be the badKing address');
-  });
+  it('should steal all the money from Reentrance contract', async () => {
+    // Amount transferred initially to Reentrance contract
+    const reentranceInitialBalance = await web3.eth.getBalance(targetContract.address);
+    assert.equal(reentranceInitialBalance, initialBalance, 'reentrance should be the 1 ether');
 
-  it('should not change the king to owner address after taking the throne, muajajajaja', async () => {
-    const prize = web3.utils.toWei('1');
-    const oldKing = await king._king();
-    assert.equal(oldKing, badKing.address, 'oldKing should be the badKing address');
-    // With value
-    await catchRevert(king.sendTransaction({ from: owner, value: prize }));
-    // Without value
-    await catchRevert(king.sendTransaction({ from: owner, value: 0 }));
-    const newKing = await king._king();
-    assert.equal(newKing, badKing.address, 'newKing should be the badKing address');
+    // Amount stated as 'steps' to steal money
+    const solDonation = await solContract.amount();
+    assert.equal(solDonation, steal, 'solDonation should be the steal amount');
+
+    // Donate money to Reentrance contract
+    await targetContract.donate(solContract.address, { value: steal });
+    const donationResult = await targetContract.balanceOf(solContract.address);
+    assert.equal(donationResult.toString(), steal, 'donationResult should be the donation amount');
+
+    // Attack!!
+    await solContract.stealMoney();
+    const targetFinalBalance = await web3.eth.getBalance(targetContract.address);
+    const solFinalBalance = await web3.eth.getBalance(solContract.address);
+    assert.equal(targetFinalBalance.toString(), '0', 'targetFinalBalance should be 0');
+    assert.equal(solFinalBalance.toString(), totalBounty, 'donationResult should be the donation amount');
+
+    // Get contract balances from balancesOf
+    const solBalanceOf = await targetContract.balanceOf(solContract.address);
+    assert.equal(solBalanceOf.toString(), '0', 'solBalanceOf should be 0');
   });
 });
